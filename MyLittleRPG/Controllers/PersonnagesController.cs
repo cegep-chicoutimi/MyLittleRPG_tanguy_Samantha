0f96aaf9f8bc98a99562a06566dbf5f028e074c9
Tanguy Caprace <2534016@etu.cchic.ca>
2025-10-22 10:20:18 -0400

    debug redreation Personnage on login

--- MyLittleRPG/Controllers/PersonnagesController.cs
+++ MyLittleRPG/Controllers/PersonnagesController.cs
@@ -259,6 +259,9 @@ namespace MyLittleRPG.Controllers
             {
                 return NotFound("L'id de l'utilisateur n'est pas valide");
             }
+            var persoexist = await _context.Personnages.FirstOrDefaultAsync(p => p.UtilisateurId == persoDto.IdUser);
+            if (persoexist != null)
+                return Ok(persoexist);
 
             if (string.IsNullOrEmpty(persoDto.Nom)) return BadRequest(new { message = "Le nom de l'utilisateur ne doit pas être vide" });
             Random random = new Random();
