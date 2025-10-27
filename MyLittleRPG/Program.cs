17e4a04e518e0f25b898df5ad0dce61907d20dce
Tanguy Caprace <2534016@etu.cchic.ca>
2025-10-27 09:50:08 -0400

    ajout TileControllerTest

--- MyLittleRPG/Program.cs
+++ MyLittleRPG/Program.cs
@@ -2,48 +2,54 @@ using Microsoft.EntityFrameworkCore;
 using MyLittleRPG.Data.Context;
 using MyLittleRPG.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddCors(opt =>
+public class Program
 {
-    opt.AddPolicy("CorsPolicy", policyBuilder =>
+    private static void Main(string[] args)
     {
-        policyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("*");
-    });
-});
+        var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+        builder.Services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", policyBuilder =>
+            {
+                policyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("*");
+            });
+        });
+
+        // Add services to the container.
 
 
-builder.Services.AddDbContext<MonsterContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("Default");
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-});
+        builder.Services.AddDbContext<MonsterContext>(options =>
+        {
+            var connectionString = builder.Configuration.GetConnectionString("Default");
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        });
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-builder.Services.AddHostedService<MonstreMaintenanceService>();
+        builder.Services.AddHostedService<MonstreMaintenanceService>();
 
-var app = builder.Build();
+        var app = builder.Build();
 
- //Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        //Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseCors("CorsPolicy");
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+
+        app.Run();
+    }
 }
-
-app.UseCors("CorsPolicy");
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-
-app.Run();
---
+++ TestMyLittleRPG/TileControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLittleRPG.Tests
+{
+    public class TileControllerTest
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+
+        public TileControllerTest(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetTuiles_WithAuthenticatedUser_Returns3x3Grid()
+        {
+            Assert.True(true);
+
+
+        }
+    }
+}
