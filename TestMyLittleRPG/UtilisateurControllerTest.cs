2ec9b0a4b20fc1393435af454d451d52f0e6b719
dev-2032527 <2032527@etu.cchic.ca>
2025-10-27 08:59:35 -0400

    Initialisation Projet Test

---
+++ TestMyLittleRPG/UtilisateurControllerTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace TestMyLittleRPG
+{
+    public class UtilisateurControllerTest :IClassFixture<WebApplicationFactory<Program>>
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
